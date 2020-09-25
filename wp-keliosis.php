a98fd96e4fe8f5678daa886f987c2354785461c4
Florian Valois <contact@florian-valois.com>
2020-09-25 12:50:24 +0200

    Init plugin

---
+++ wp-keliosis.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Plugin Name: WP Keliosis
+ * Plugin URI: https://wordpress.org/plugins/wp-keliosis/
+ * Description: The best all-in-one plugin to customize default options of WordPress.
+ * Version: 0.2.4
+ * Author: Florian Valois
+ * Author URI: https://florian-valois.com
+ * Licence: GPLv2 or later
+ *
+ * Text Domain: keliosis
+ * Domain Path: /languages
+ */
+
+defined('ABSPATH') or die('Hey, you can\'t access this file, you silly human !');
