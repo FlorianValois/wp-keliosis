555ec8a8827cc4258a6b24b75d70928f7c6a6d8e
Florian Valois <contact@florian-valois.com>
2020-09-25 12:57:18 +0200

    Update wp-keliosis.php
    - Add constants
    - Add translation function

--- wp-keliosis.php
+++ wp-keliosis.php
@@ -3,7 +3,7 @@
  * Plugin Name: WP Keliosis
  * Plugin URI: https://wordpress.org/plugins/wp-keliosis/
  * Description: The best all-in-one plugin to customize default options of WordPress.
- * Version: 0.2.4
+ * Version: 0.3
  * Author: Florian Valois
  * Author URI: https://florian-valois.com
  * Licence: GPLv2 or later
@@ -13,3 +13,34 @@
  */
 
 defined('ABSPATH') or die('Hey, you can\'t access this file, you silly human !');
+
+// Keliosis defines
+define( 'WP_KELIOSIS_VERSION',                  '0.3' );
+define( 'WP_KELIOSIS_NAME',                     'keliosis' );
+define( 'WP_KELIOSIS_SLUG',                     'wp-keliosis' );
+define( 'WP_KELIOSIS_FILE',                     __FILE__ );
+define( 'WP_KELIOSIS_PATH',                     realpath( plugin_dir_path( WP_KELIOSIS_FILE ) ) . '/' );
+
+define( 'WP_KELIOSIS_PATH_INC',                 realpath( WP_KELIOSIS_PATH . 'inc/' ) . '/' );
+define( 'WP_KELIOSIS_PATH_VIEWS',               realpath( WP_KELIOSIS_PATH . 'views/' ) . '/' );
+
+define( 'WP_KELIOSIS_URL',                      plugin_dir_url( WP_KELIOSIS_FILE ) );
+define( 'WP_KELIOSIS_URL_ASSETS',               WP_KELIOSIS_URL . 'assets/' );
+
+/**
+ * Loads WP Keliosis translations
+ *
+ * @since 0.1
+ * @author Florian Valois
+ *
+ * @return void
+ */
+if ( !function_exists( 'keliosis_load_textdomain' ) ) {
+  add_action( 'plugins_loaded', 'keliosis_load_textdomain' );
+  function keliosis_load_textdomain() {
+    $locale = get_locale();
+    $locale = apply_filters( 'plugin_locale', $locale, 'keliosis' );
+    load_textdomain( 'keliosis', WP_LANG_DIR . '/plugins/wp-keliosis-' . $locale . '.mo' );
+    load_plugin_textdomain( 'keliosis', false, dirname( plugin_basename( __FILE__ ) ) . '/lang/' );
+  }
+}
