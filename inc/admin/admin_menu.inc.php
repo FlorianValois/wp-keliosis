c226ae8193a1c1ded6859ad610df4d2ee21fc25f
Florian Valois <contact@florian-valois.com>
2020-09-30 14:52:04 +0200

    Update admin_menu.inc.php
    Add dynamic load of css and js

--- inc/admin/admin_menu.inc.php
+++ inc/admin/admin_menu.inc.php
@@ -8,50 +8,60 @@ defined('ABSPATH') or die('Hey, you can\'t access this file, you silly human !')
  * @since 0.1
  * @author Florian Valois
  */
-if ( !function_exists( 'keolis_link_dashboard' ) ) 
+if ( !function_exists( 'keliosis_link_dashboard' ) ) 
 {
-  add_action('admin_menu','keolis_link_dashboard');
-  function keolis_link_dashboard()
+  add_action('admin_menu','keliosis_link_dashboard');
+  function keliosis_link_dashboard()
   {
-    $keliosis_admin_page = add_menu_page(
-      __( 'WP Keliosis', 'keliosis' ), 
-      __( 'WP Keliosis', 'keliosis' ), 
-      'manage_options', 
-      'wp_keliosis', 
-      'keolis_dashboard', 
-      'dashicons-admin-plugins',  
-      99 
-    );
-    
-    // Dashboard
-    add_submenu_page( 
-      'wp_keliosis', 
-      __( 'Dashboard', 'keliosis' ), 
-      __( 'Dashboard', 'keliosis' ),
-      'manage_options', 
-      'wp_keliosis',
-      'keolis_dashboard'
-    );
-    
-    // Images
-    add_submenu_page( 
-      'wp_keliosis', 
-      __( 'Images', 'keliosis' ), 
-      __( 'Images', 'keliosis' ),
-      'manage_options', 
-      'wp_keliosis_images',
-      'keolis_images'
-    );
-    
-    // Changelog
-    add_submenu_page( 
-      'wp_keliosis', 
-      __( 'Changelog', 'keliosis' ), 
-      __( 'Changelog', 'keliosis' ),
-      'manage_options',
-      'wp_keliosis_changelog',
-      'keolis_changelog'
-    );
+    $keliosis_admin_page = array(
+      
+      // Main plugin page
+      add_menu_page(
+        __( 'WP Keliosis', 'keliosis' ), 
+        __( 'WP Keliosis', 'keliosis' ), 
+        'manage_options', 
+        'wp_keliosis', 
+        'keliosis_dashboard', 
+        'dashicons-admin-plugins',  
+        99 
+      ),
 
+      // Dashboard
+      add_submenu_page( 
+        'wp_keliosis', 
+        __( 'Dashboard', 'keliosis' ), 
+        __( 'Dashboard', 'keliosis' ),
+        'manage_options', 
+        'wp_keliosis',
+        'keliosis_dashboard'
+      ),
+
+      // Images
+      add_submenu_page( 
+        'wp_keliosis', 
+        __( 'Images', 'keliosis' ), 
+        __( 'Images', 'keliosis' ),
+        'manage_options', 
+        'wp_keliosis_images',
+        'keliosis_images'
+      ),
+
+      // Changelog
+      add_submenu_page( 
+        'wp_keliosis', 
+        __( 'Changelog', 'keliosis' ), 
+        __( 'Changelog', 'keliosis' ),
+        'manage_options',
+        'wp_keliosis_changelog',
+        'keliosis_changelog'
+      )
+    );
+    
+
+    // Load CSS and JS only on plugin page
+    foreach($keliosis_admin_page as $key){ 
+      add_action('load-'.$key, 'keliosis_load_enqueue_style_script');
+    }
+    
   }
 }
