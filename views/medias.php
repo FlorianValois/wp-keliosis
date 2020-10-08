43457fd2c066566f60096f65d0f665e3dc2032e2
Florian Valois <contact@florian-valois.com>
2020-10-08 16:50:51 +0200

    Update medias.php
    debug html

--- views/medias.php
+++ views/medias.php
@@ -29,16 +29,16 @@ if ( !function_exists( 'wpk__medias' ) )
         <div class="wpk-card">
           <h2 class="title"><?php echo __('Rewrite filename', 'keliosis'); ?></h2>
           <div class="content">
-            <fieldset class="d-flex align-items-center">
+            <div class="check-group">
               <label for="medias__rewrite_filename">
                 <input id="medias__rewrite_filename" class="form-control" type="checkbox" name="medias__rewrite_filename" value="1" <?php checked( wpk_options('medias__rewrite_filename'), 1 ); ?>>
                 <?php echo __('Active this option to rewrite filename during upload.', 'keliosis'); ?>
               </label>
-            </fieldset>
-            <p class="description">
-                <?php echo __('Enabling this option to turn filename into a web optimized filename.', 'keliosis'); ?><br>
-                <?php echo __('Example : F@_pf42çfèdg.JPG in f-pf42cfedg.jpg', 'keliosis'); ?>
-            </p>
+              <p class="description">
+                  <?php echo __('Enabling this option to turn filename into a web optimized filename.', 'keliosis'); ?><br>
+                  <?php echo __('Example : F@_pf42çfèdg.JPG in f-pf42cfedg.jpg', 'keliosis'); ?>
+              </p>
+            </div>
           </div>
         </div>
       </div>
